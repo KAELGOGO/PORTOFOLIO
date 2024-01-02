9c437e4b7d8d924fab26cf2e31734c6fdb105866
KELVIN LEANDI <133320677+KAELGOGO@users.noreply.github.com>
2024-01-02 16:43:41 +0700

    Add files via upload

--- portofolio/script.js
+++ portofolio/script.js
@@ -1,3 +1,13 @@
 function myalert() {
   alert("SORRY, THE PAGE IS IN PROCESS");
 }
+
+// Fungsi untuk membuka Pop-up
+function openPopUpAbout() {
+  document.getElementById("about").style.display = "block";
+}
+
+// Fungsi untuk menutup Pop-up
+function closeAbout() {
+  document.getElementById("about").style.display = "none";
+}
